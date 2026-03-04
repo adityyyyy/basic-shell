51d817cfaf94499bf5a02715ba85739eccdf9317
Aditya <apatel@hiivehealth.com>
2026-03-05 01:56:28 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,45 @@
-use std::io::{self, Write};
+use std::{
+    env, fs,
+    io::{self, Write},
+    path::{Path, PathBuf},
+};
+
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+
+fn find_executable(cmd: &str) -> Option<PathBuf> {
+    if cmd.starts_with("/") {
+        let path = Path::new(cmd);
+        if is_executable(path) {
+            return Some(path.to_path_buf());
+        }
+        return None;
+    }
+
+    let paths = env::var_os("PATH")?;
+    for dir in env::split_paths(&paths) {
+        let full_path = dir.join(cmd);
+        if is_executable(&full_path) {
+            return Some(full_path);
+        }
+    }
+
+    None
+}
+
+fn is_executable(path: &Path) -> bool {
+    if let Ok(metadata) = fs::metadata(path) {
+        if metadata.is_file() {
+            #[cfg(unix)]
+            {
+                let mode = metadata.permissions().mode();
+                return mode & 0o111 != 0;
+            }
+        }
+    }
+
+    false
+}
 
 fn main() {
     let mut buf = String::new();
@@ -30,8 +71,12 @@ fn main() {
                 if let Some(cmd) = args.first() {
                     if available_commands.contains(cmd) {
                         println!("{cmd} is a shell builtin");
+                        continue;
+                    }
+                    if let Some(path) = find_executable(cmd) {
+                        println!("{} is {}", cmd, path.display());
                     } else {
-                        println!("{cmd}: not found");
+                        println!("{}: not found", cmd);
                     }
                 }
             }
