4266d85f200437eeb6d72893d4945effeb69fd43
Aditya <apatel@hiivehealth.com>
2026-03-04 23:47:30 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
     let mut buf = String::new();
 
     loop {
+        buf.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut buf).unwrap();
