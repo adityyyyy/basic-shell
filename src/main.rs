a23340acb82c3c7eafd1603d77b0383798a74311
Aditya <apatel@hiivehealth.com>
2026-03-05 00:17:49 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,28 @@ fn main() {
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut buf).unwrap();
         let input = buf.trim();
-        if input == "exit" {
-            break;
-        }
-        if input.starts_with("echo ") {
-            println!("{}", &input[5..]);
-        } else {
-            println!("{}: command not found", buf.trim());
-        }
+
+        let available_commands = ["exit", "echo", "type"];
+
+        let command = input.split_once(" ").map(|v| v.0).unwrap();
+
+        match command {
+            "exit" => {
+                break;
+            }
+            "echo" => {
+                println!("{}", &input[5..]);
+            }
+            "type" => {
+                if available_commands.contains(&&input[5..]) {
+                    println!("{} is a shell builtin", &input[5..]);
+                } else {
+                    println!("{}: not found", &input[5..]);
+                }
+            }
+            _ => {
+                println!("{command}: command not found");
+            }
+        };
     }
 }
